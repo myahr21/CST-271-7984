6cf7bdf493ae58bb532ee6970f48e0ea4a021745
root <root@paloaltonetworks.com>
2020-09-29 20:04:04 +0000

    Updated script output messaging to be user friendly

--- bruteforce.sh
+++ bruteforce.sh
@@ -3,6 +3,7 @@
 echo "Executing FTP Brute Force Login: 15 attempts, 500ms interval"
 for i in {1..15}
 do
-  curl -s -m 0.5 ftp://admin:badpass@192.168.50.10
+  curl -s -m 0.5 ftp://admin:badpass$i@192.168.50.10
+  echo "FTP Brute Force Attempt: $i of 15"
 done
 echo "Completed. Please allow 1 minute for Palo Alto threat logs to be generated."
