9fcb41a44345b81c791052c7d3afb4b24ea5181e
lab-user <lab-user@paloaltonetworks.com>
2020-10-05 15:05:36 +0000

    Fixed smbpassword options for password change

--- fixldap.sh
+++ fixldap.sh
@@ -4,4 +4,4 @@ pass=Pal0Alt0
 ldap_user=lab-user-id
 
 echo "Resetting Password for lDAP User: $ldap_user"
-(echo "$pass"; echo "$pass") | smbpasswd -s -a $ldap_user
+(echo "$pass"; echo "$pass") | smbpasswd -s -U $ldap_user
