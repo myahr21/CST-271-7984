d5f3086403048366698d899e911fb724404995e8
lab-user <lab-user@paloaltonetworks.com>
2020-10-09 15:42:09 +0000

    Added pw reset for lab-user ldap user account

--- fixldap.sh
+++ fixldap.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 pass=Pal0Alt0
-ldap_user=lab-user-id
+lab9_ldap_user=lab-user-id
+lab10_ldap_user=lab-user
 
-echo "Resetting Password for lDAP User: $ldap_user"
-(echo "$pass"; echo "$pass") | smbpasswd -s -U $ldap_user
+echo "Resetting Password for lDAP User: $lab9_ldap_user"
+(echo "$pass"; echo "$pass") | smbpasswd -s -U $lab9_ldap_user
+
+echo "Resetting Password for lDAP User: $lab10_ldap_user"
+(echo "$pass"; echo "$pass") | smbpasswd -s -U $lab10_ldap_user
